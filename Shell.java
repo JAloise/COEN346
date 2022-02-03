b3278e4fb3bc09c2a66686152867dce18d36cc70
karim-tabbara <97918808+karim-tabbara@users.noreply.github.com>
2022-02-03 16:19:52 -0500

    Updated new shell (echo/exit working)

--- Shell.java
+++ Shell.java
@@ -15,7 +15,8 @@ public class Shell extends Thread {
     }
 
     public void run() {
-        while(true) {
+        boolean cond = true;
+    	while(cond == true) {
             System.out.print(User+"@"+Host+"$ ");
             String input = sc.nextLine();
             
@@ -24,40 +25,29 @@ public class Shell extends Thread {
                 char c = input.charAt(i);
                 command = command + c;
             }
+            
+        
 
             switch (command) {
-                case "echo":  
-                /*String message = "";
-                for(int i = 0 ; i < input.length() ;i++) {
-                    char c = input.charAt(i); 
-                    if(c == '"') {
-                        for(int j = i ; j < input.length() ;j++) { 
-                            if(c != '"') {
-                                message = message + c;
-                            }
-                        }
-                    }
-                }
-                System.out.print(message);        
-                    continue; */
-                	
-                //String message = "";
-                String toAdd = input;
-                //message += toAdd;
-                String toDisplay = "";
-                for(int i = 6 ; i < toAdd.length()-1 ; i++) {
-                	char c = toAdd.charAt(i);
-                	toDisplay += c;
-                }
-                System.out.println(toDisplay);
-                continue;
+                
+            	case "echo":  
+            		String toAdd = input;
+            		String toDisplay = "";
+            		for(int i = 6 ; i < toAdd.length()-1 ; i++) {
+            			char c = toAdd.charAt(i);
+            			toDisplay += c;
+            		}
+            		System.out.println(toDisplay);
+            		continue;
                     
-                case "exit":  command = "exit";
+                case "exit":  
+                         cond = false;
                          break;
 
-                default: command = "Invalid command";
-                         continue;
+                default: 
+                		command = "Invalid command";
+                        continue;
             }       
-            }  
+          }  
         }
 }
